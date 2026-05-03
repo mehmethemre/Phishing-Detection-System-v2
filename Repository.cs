d9604a11a6df421a514d1ef5c93a102ff3cc7a77
Mehmet Emre Erdoğan <mehmeterdoogaan@gmail.com>
2026-05-03 23:40:45 +0300

    KAN-13 ORM ve Generic Repository altyapisi kuruldu

---
+++ Repository.cs
@@ -0,0 +1,37 @@
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Repository<T> : IRepository<T> where T : class
+{
+    private readonly AppDbContext _context;
+    private readonly DbSet<T> _dbSet;
+
+    public Repository(AppDbContext context)
+    {
+        _context = context;
+        _dbSet = context.Set<T>();
+    }
+
+    public IEnumerable<T> GetAll() => _dbSet.ToList();
+    
+    public T GetById(int id) => _dbSet.Find(id);
+    
+    public void Add(T entity)
+    {
+        _dbSet.Add(entity);
+        _context.SaveChanges();
+    }
+
+    public void Update(T entity)
+    {
+        _dbSet.Update(entity);
+        _context.SaveChanges();
+    }
+
+    public void Delete(T entity)
+    {
+        _dbSet.Remove(entity);
+        _context.SaveChanges();
+    }
+}

a3121932c2c758cf6e925d52c601787176e0f08c
Mehmet Emre Erdoğan <mehmeterdoogaan@gmail.com>
2026-05-27 01:56:00 +0300

    AppDbContext kurucu metodu (constructor) eklendi

--- AppDbContext.cs
+++ AppDbContext.cs
@@ -2,6 +2,10 @@ using Microsoft.EntityFrameworkCore;
 
 public class AppDbContext : DbContext
 {
+
+      public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+{
+}
     public DbSet<User> Users { get; set; }
     public DbSet<Document> Documents { get; set; }
 
